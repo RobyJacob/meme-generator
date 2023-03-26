92838c7340781bc1c6cfe511ffe62413e3859aef
roby <robyjacob1998@gmail.com>
2023-03-27 00:07:21 +0530

    fetch meme data from api

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,65 @@
-import memesData from "../memesData"
 import Meme from "./Meme"
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 export default function Form() {
-    const [memImage, setMemeImage] = useState('')
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        imgUrl: ''
+    })
+    const [allMemes, setAllMemes] = useState([])
 
     function displayMeme() {
-        const memes = memesData.data.memes
-        const randomMeme = memes[Math.floor(Math.random() * memes.length)]
-        setMemeImage(randomMeme.url)
+        const randomMeme = allMemes[Math.floor(Math.random() * allMemes.length)]
+        setMeme(prevState => ({
+            ...prevState,
+            imgUrl: randomMeme.url
+        }))
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+
+        setMeme(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     return (
         <div className='form'>
             <div className='form-textfields'>
-                <input className='form-text' type='text' placeholder='top text'/>
-                <input className='form-text' type='text' placeholder='bottom text'/>
+                <input 
+                    className='form-text' 
+                    type='text' 
+                    placeholder='top text'
+                    name='topText'
+                    value={meme.topText}
+                    onChange={handleChange}
+                    />
+                <input 
+                    className='form-text' 
+                    type='text' 
+                    placeholder='bottom text'
+                    name='bottomText'
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
             </div>
             <button className='form-button' onClick={displayMeme}>
                 Get a new meme image
             </button>
-            <Meme imgUrl={memImage} />
+            <Meme 
+                imgUrl={meme.imgUrl} 
+                topText={meme.topText}
+                bottomText={meme.bottomText}
+                />
         </div>
     )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,9 @@
 export default function Meme(props) {
     return (
         <div className='meme'>
+            <h1 className='meme-text top'>{props.topText}</h1>
             <img src={props.imgUrl} className='meme-img' />
+            <h1 className='meme-text bottom'>{props.bottomText}</h1>
         </div>
     )
 }
